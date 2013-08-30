f2a25b1e516ba19fdf6bd9588c61bfae55306297
Spencer Krum <skrum@go2uti.com>
2013-08-30 14:48:23 -0700

    confining user and group to linux

--- lib/facter/user_group.rb
+++ lib/facter/user_group.rb
@@ -1,12 +1,14 @@
 
 require 'etc'
 Facter.add("puppet_user") do
+  confine :kernel => "Linux"
   setcode do
     Etc.getpwuid(Process.uid).name
   end
 end
 
 Facter.add("puppet_group") do
+  confine :kernel => "Linux"
   setcode do
     Etc.getgrgid(Process.gid).name
   end
